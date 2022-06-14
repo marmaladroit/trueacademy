b3d782b02aad00781a1be6a31c420313de4fcebd
marmaladroit <m.a.sergeenko@inbox.ru>
2022-06-14 21:57:57 +0300

    est

--- academy/src/main/java/by/academy/lesson2/Application.java
+++ academy/src/main/java/by/academy/lesson2/Application.java
@@ -3,18 +3,17 @@ package by.academy.lesson2;
 public class Application {
 
 	public static void main(String[] args) {
-	 Cat anon = new Cat();
-	 anon.age = 1;
-	 Cat shumi = new Cat(); 
-	 shumi.nickname = "Shumi";
-	 
-	 anon.grow();
-	 anon.grow();
-	 anon.grow();
-	 shumi.eat();
-	 shumi.sleep();
-	 shumi.walk();
-	 
+		Cat anon = new Cat();
+		anon.age = 1;
+		Cat shumi = new Cat();
+		shumi.nickname = "Shumi";
 
-    }
+		anon.grow();
+		anon.grow();
+		anon.grow();
+		shumi.eat();
+		shumi.sleep();
+		shumi.walk();
+
+	}
 }
