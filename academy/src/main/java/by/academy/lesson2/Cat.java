9ad4f2158e913320aa32a6c89b1fa0b81bd645dc
marmaladroit <m.a.sergeenko@inbox.ru>
2022-06-14 22:09:43 +0300

    test

--- academy/src/main/java/by/academy/lesson2/Cat.java
+++ academy/src/main/java/by/academy/lesson2/Cat.java
@@ -34,15 +34,15 @@ class Cat {
 	}
 
 	public void sleep() {
-		System.out.println("Кот спит");
+		System.out.println("РљРѕС‚ СЃРїРёС‚");
 	}
 
 	public void eat() {
-		System.out.println("Кот ест");
+		System.out.println("РљРѕС‚ РµСЃС‚");
 	}
 
 	public void walk() {
-		System.out.println("Кот гуляет");
+		System.out.println("РљРѕС‚ РіСѓР»СЏРµС‚");
 	}
 
 	public double setMoney(double money) {
